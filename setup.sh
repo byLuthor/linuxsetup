609f8aa3a88469eeb8bf26edb19cb7ba8f670a47
Luthor <173418647+byLuthor@users.noreply.github.com>
2024-06-22 04:37:42 -0500

    Add interactive functionality & sys_upgrade function
    Changes to [setup.sh]
    - Add user interaction capabilities
        - Use a WHILE loop as the MENU (PHASE) Selector
    
    - Create sys_upgrade function
        - Define /etc/apt/sources.list as APT_SOURCES_FILE
        - Create logic to open & modify APT_SOURCES_LIST with Neovim or Nano
        - Update and Upgrade APT Packages

--- setup.sh
+++ setup.sh
@@ -8,6 +8,39 @@ GREEN='\e[32m'
 
 LINUXSETUPDIR="$HOME/linuxsetup"
 
+sys_upgrade() {
+	APT_SOURCES_FILE="/etc/apt/sources.list"
+
+	echo -e "${YELLOW}Initializing...${RC}"
+
+	# Check if file exists
+	if [ -f "$APT_SOURCES_FILE" ]; then
+		echo -e "${GREEN}Opening file for modification: $APT_SOURCES_FILE"
+		
+		if command -v nvim &> /dev/null; then
+			nvim "$APT_SOURCES_FILE"
+		else
+			nano "$APT_SOURCES_FILE"
+		fi
+
+		echo -e "${GREEN}File modification completed.${RC}"
+
+	else
+		echo -e "${RED}Error: File $APT_SOURCES_FILE does not exist.${RC}"
+		exit 1
+	fi
+
+	sudo apt update && sudo apt upgrade
+
+	if [[ $? -eq 0 ]]; then
+		echo -e "${GREEN}PHASE 1 Complete.${RC}"
+	else
+		echo -e "${RED}Failed to upgrade system.${RC}"
+		exit 1
+	fi
+
+}
+
 conf_environment() {
 	if [[ ! -d "$LINUXSETUPDIR" ]]; then
 		echo -e "${YELLOW}Creating Linux Setup Directory: $LINUXSETUPDIR${RC}"
@@ -35,5 +68,24 @@ show_menu() {
 	echo "3) Exit"
 }
 
-show_menu
+# Main script loop
+while true; do
+	show_menu
+	read -p "Enter your choice [1-4]: " choice
+
+	case $choice in
+		1)
+			sys_upgrade
+			break
+			;;
+		*)
+			echo -e "${RED}Invalid choice. Please enter a number between 1 and 3.${RC}"
+			;;
+	esac
+
+	echo "Press Enter to continue..."
+	read
+done
+
+
 
