ba01b81560fdd2c8f4cff9e049dbc7094dbdb814
Luthor <173418647+byLuthor@users.noreply.github.com>
2024-06-22 03:48:03 -0500

    Add Select MENU
    Changes to [setup.sh]
    - Create conf_environment function
    - Move previous script segments into conf_environment function
        - Moved DIR creation and repo CLONE segmets
    
    - Create Select MENU

--- setup.sh
+++ setup.sh
@@ -8,21 +8,32 @@ GREEN='\e[32m'
 
 LINUXSETUPDIR="$HOME/linuxsetup"
 
-if [[ ! -d "$LINUXSETUPDIR" ]]; then
-	echo -e "${YELLOW}Creating Linux Setup Directory: $LINUXSETUPDIR${RC}"
-	mkdir -p "$LINUXSETUPDIR"
-	echo -e "${GREEN}Linux Setup directory created: $LINUXSETUPDIR${RC}"
-fi
-
-if [[ ! -d "$LINUXSETUPDIR/dotfiles" ]]; then
-	echo -e "${YELLOW}Cloning dotfiles repository into: $LINUXSETUPDIR/dotfiles${RC}"
-	git clone https://github.com/byLuthor/dotfiles.git
-	if [[ $? -eq 0 ]]; then
-		echo -e "${GREEN}Successfully cloned dotfiles repositoy${RC}"
-	else
-		echo -e "${RED}Failed to clone dotfiles repository${RC}"
-		exit 1
+conf_environment() {
+	if [[ ! -d "$LINUXSETUPDIR" ]]; then
+		echo -e "${YELLOW}Creating Linux Setup Directory: $LINUXSETUPDIR${RC}"
+		mkdir -p "$LINUXSETUPDIR"
+		echo -e "${GREEN}Linux Setup directory created: $LINUXSETUPDIR${RC}"
 	fi
-fi
 
+	if [[ ! -d "$LINUXSETUPDIR/dotfiles" ]]; then
+		echo -e "${YELLOW}Cloning dotfiles repository into: $LINUXSETUPDIR/dotfiles${RC}"
+		git clone https://github.com/byLuthor/dotfiles.git
+		if [[ $? -eq 0 ]]; then
+			echo -e "${GREEN}Successfully cloned dotfiles repositoy${RC}"
+		else
+			echo -e "${RED}Failed to clone dotfiles repository${RC}"
+			exit 1
+		fi
+	fi
+}
+
+# Display the menu
+show_menu() {
+	echo -e "${YELLOW}Select a Setup Phase:${RC}"
+	echo "1) Modify APT sources & upgrade system"
+	echo "2) Install dependencies & setup environment"
+	echo "3) Exit"
+}
+
+show_menu
 
